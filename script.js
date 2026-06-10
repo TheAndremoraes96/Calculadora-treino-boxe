2cfdaecd166e0a9cda128877a960c900e4c77d94
André Moraes <prettyboymoraes@gmail.com>
2026-06-10 19:59:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 function calcularIMC() {
 
     let peso = document.getElementById("peso").value;
-
     let altura = document.getElementById("altura").value;
 
     let imc = peso / (altura * altura);
 
-    document.getElementById("resultado").innerHTML =
-        "Seu IMC é: " + imc.toFixed(2);
+    let classificacao = "";
 
+    if (imc < 18.5) {
+        classificacao = "Abaixo do peso";
+    } else if (imc < 25) {
+        classificacao = "Peso normal";
+    } else if (imc < 30) {
+        classificacao = "Sobrepeso";
+    } else {
+        classificacao = "Obesidade";
+    }
+
+    document.getElementById("resultado").innerHTML =
+        "IMC: " + imc.toFixed(2) +
+        "<br>Classificação: " + classificacao;
 }
